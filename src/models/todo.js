12d9f6d46f5e8a231771beeeb69a96af6127a194
ha-matsumu <ha.matsumu@gmail.com>
2019-01-17 11:46:51 +0900

    modified todo.js

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
     title: DataTypes.STRING,
-    body: DataTypes.STRING,
+    body: DataTypes.TEXT,
     completed: DataTypes.BOOLEAN
   }, {});
   Todo.associate = function(models) {
