16f1b1ce79660379782337f70d2c87e833aabddc
ha-matsumu <ha.matsumu@gmail.com>
2019-01-19 17:59:54 +0900

    modified api.router.js

--- src/resources/api.router.js
+++ src/resources/api.router.js
@@ -4,9 +4,10 @@ const apiController = require("./api.controller");
 
 // routing用（api/todos）のmiddlewareの作成
 // リクエスト（HTTPメソッド）、path、メソッド（callback関数）
-router.route("/todos/:id")
+router.route("/todos")
   .get(apiController.getTodos)
-  .post(apiController.postTodos)
+  .post(apiController.postTodos);
+router.route("/todos/:id")
   .put(apiController.putTodos)
   .delete(apiController.deleteTodos);
 
