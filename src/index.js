a5509d3c4cd963aeb4cf44400618b23e88a32265
ha-matsumu <ha.matsumu@gmail.com>
2019-01-19 17:23:41 +0900

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const app = require("./server");
 const chalk = require("chalk");
+const portNumber = 3000;
 
 // サーバーを始動し、ポート3000で接続
-app.listen(3000, () => {
-  console.log("Trivial quiz app listening on port", chalk.green("3000"), "!");
+app.listen(portNumber, () => {
+  console.log("Trivial quiz app listening on port", chalk.green(portNumber), "!");
 });
