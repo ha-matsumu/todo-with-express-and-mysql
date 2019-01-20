a381c4e6a3918d3a6bab913a2a9e9f2e880bda97
ha-matsumu <ha.matsumu@gmail.com>
2019-01-20 11:40:34 +0900

    added message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const app = require("./server");
 const chalk = require("chalk");
 const portNumber = 3000;
 
+const url = `http://localhost:${portNumber}/api/todos`;
+
 // サーバーを始動し、ポート3000で接続
 app.listen(portNumber, () => {
-  console.log("Trivial quiz app listening on port", chalk.green(portNumber), "!");
+  console.log("Trivial quiz app listening on port", portNumber, "!");
+  console.log(
+    "Access to",
+    chalk.black.bgWhite(url),
+    "from your browser or Postman."
+  );
 });
