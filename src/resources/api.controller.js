c5463e1bcafc370f367c028e00f01c54385c0f7c
ha-matsumu <ha.matsumu@gmail.com>
2019-01-20 16:55:10 +0900

    modified api.controller.js

--- src/resources/api.controller.js
+++ src/resources/api.controller.js
@@ -3,12 +3,15 @@ const Todo = require("../models/index").Todo;
 module.exports = {
   // 各リクエストに対して実行されるメソッドを定義
   async getTodos(req, res) {
-    // select * from Todo order by id;
-    const todos = await Todo.findAll({
-      order: [["id", "ASC"]]
-    });
-
-    res.status(200).send(todos);
+    try {
+      // select * from Todo order by id;
+      const todos = await Todo.findAll({
+        order: [["id", "ASC"]]
+      });
+      res.status(200).json(todos);
+    } catch(error) {
+      res.json(error);
+    }
   },
   postTodos(req, res) {
     res.status(200).send("create todo to DB");
