db68386d36439b5120f96245141534d1aaef871b
ha-matsumu <ha.matsumu@gmail.com>
2019-01-22 19:03:47 +0900

    modified getTodos.spec.js

--- test/src/resources/api.router/getTodos.spec.js
+++ test/src/resources/api.router/getTodos.spec.js
@@ -3,7 +3,8 @@ const requestHelper = require("../requestHelper");
 
 describe("GET /api/todos", () => {
   it("API経由で取得したデータの確認", () => {
-    return requestHelper.requestAPI("get", "/api/todos", 200)
+    return requestHelper
+      .requestAPI("get", "/api/todos", 200)
       .set("Accept", "application/json")
       .then(response => {
         const todos = response.body;
@@ -11,12 +12,32 @@ describe("GET /api/todos", () => {
         assert.equal(Array.isArray(todos), true, "配列ではありません。");
         todos.forEach((todo, index) => {
           // DBの各カラムのデータの確認
-          assert.equal(todo.id, index+1, "idが正しくありません。");
-          assert.equal(typeof todo.title, "string", "titleは'number'型ではありません。");
-          assert.equal(typeof todo.body, "string", "bodyは'number'型ではありません。");
-          assert.equal(todo.completed, false, "completedは'number'型ではありません。");
-          assert.equal(typeof todo.createdAt, "string", "createdAtは'number'型ではありません。");
-          assert.equal(typeof todo.updatedAt, "string", "updatedAtは'number'型ではありません。");
+          assert.equal(todo.id, index + 1, "idが正しくありません。");
+          assert.equal(
+            typeof todo.title,
+            "string",
+            "titleは'string'型ではありません。"
+          );
+          assert.equal(
+            typeof todo.body,
+            "string",
+            "bodyは'string'型ではありません。"
+          );
+          assert.equal(
+            todo.completed,
+            false,
+            "completedは'false'ではありません。"
+          );
+          assert.equal(
+            typeof todo.createdAt,
+            "string",
+            "createdAtは'string'型ではありません。"
+          );
+          assert.equal(
+            typeof todo.updatedAt,
+            "string",
+            "updatedAtは'string'型ではありません。"
+          );
         });
       });
   });
