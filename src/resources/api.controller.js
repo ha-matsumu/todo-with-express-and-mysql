61c97ae51c8feca79650033ba6f953f9c3d02192
ha-matsumu <ha.matsumu@gmail.com>
2019-01-22 19:03:07 +0900

    modified postTodos function

--- src/resources/api.controller.js
+++ src/resources/api.controller.js
@@ -1,21 +1,41 @@
-const Todo = require("../models/index").Todo;
+const index = require("../models/index");
 
 module.exports = {
   // 各リクエストに対して実行されるメソッドを定義
   async getTodos(req, res) {
     try {
       // select * from Todo order by id;
-      const todos = await Todo.findAll({
+      const todos = await index.Todo.findAll({
         order: [["id", "ASC"]]
       });
       res.status(200).json(todos);
-    } catch(error) {
+    } catch (error) {
       res.json(error);
     }
   },
-  postTodos(req, res) {
-    res.status(200).send("create todo to DB");
+
+  async postTodos(req, res) {
+    let transaction;
+    try {
+      transaction = await index.sequelize.transaction();
+
+      // inset into Todo(title, body) values(value1, value2);
+      const todo = await index.Todo.create(
+        {
+          title: req.body.title,
+          body: req.body.body,
+          completed: req.body.completed
+        },
+        { transaction }
+      );
+      await transaction.commit();
+      res.status(200).json(todo);
+    } catch (error) {
+      await transaction.rollback();
+      res.json(error);
+    }
   },
+
   putTodos(req, res) {
     const id = req.params.id;
     const data = "update todo of id " + id + " in DB";
