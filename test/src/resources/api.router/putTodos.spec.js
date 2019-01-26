debf524212b29d12ef7c1ef34f6c41b6a14d5768
ha-matsumu <ha.matsumu@gmail.com>
2019-01-26 23:43:25 +0900

    created putTodos.spec.js

---
+++ test/src/resources/api.router/putTodos.spec.js
@@ -0,0 +1,84 @@
+const assert = require("power-assert");
+const requestHelper = require("../requestHelper");
+const todoFactory = require("../../factories/todo");
+const truncate = require("../../truncate");
+
+describe("PUT /api/todos/1", () => {
+  before(async () => {
+    const promises = [];
+    for (let i = 0; i < 5; i++) {
+      promises.push(todoFactory());
+    }
+    await Promise.all(promises);
+  });
+
+  it("更新したデータの確認（正常系）", () => {
+    return requestHelper
+      .requestAPI("put", "/api/todos/1", 200)
+      .set("Accept", "application/json")
+      .send({ title: "titleA", body: "bodyA", completed: true })
+      .then(response => {
+        assert.equal(
+          response.body.title,
+          "titleA",
+          "titleの値が正しくありません。"
+        );
+        assert.equal(
+          response.body.body,
+          "bodyA",
+          "bodyの値が正しくありません。"
+        );
+        assert.equal(
+          response.body.completed,
+          true,
+          "completedの値が正しくありません。"
+        );
+      });
+  });
+
+  it("更新したデータの確認（異常系）", () => {
+    return requestHelper
+      .requestAPI("put", "/api/todos/1", 200)
+      .set("Accept", "application/json")
+      .send({ title: titleA })
+      .then(response => {
+        console.log(response.body);
+
+        assert.equal(
+          response.body.name,
+          "SequelizeDatabaseError",
+          "データの作成に成功しています。"
+        );
+      });
+  });
+});
+
+describe("GET /api/todos/1", () => {
+  after(async () => {
+    await truncate();
+  });
+
+  it("更新したデータをDBから取得できるかの確認", () => {
+    return requestHelper
+      .requestAPI("get", "/api/todos/1", 200)
+      .set("Accept", "application/json")
+      .then(response => {
+        // DBの各カラムの値チェック
+        assert.equal(
+          response.body.title,
+          "titleA",
+          "titleの値が正しくありません。"
+        );
+        assert.equal(
+          response.body.body,
+          "bodyA",
+          "bodyの値が正しくありません。"
+        );
+        assert.equal(
+          response.body.completed,
+          true,
+          "completedの値が正しくありません。"
+        );
+      });
+  });
+});
