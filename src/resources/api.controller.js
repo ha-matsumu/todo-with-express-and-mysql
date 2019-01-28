de56e5ce3ed32c637de0c854496f721308b7ab47
ha-matsumu <ha.matsumu@gmail.com>
2019-01-28 13:02:05 +0900

    modified api.controller.js

--- src/resources/api.controller.js
+++ src/resources/api.controller.js
@@ -47,10 +47,26 @@ module.exports = {
     }
   },
 
-  putTodos(req, res) {
-    const id = req.params.id;
-    const data = "update todo of id " + id + " in DB";
-    res.status(200).send(data);
+  async putTodos(req, res) {
+    const selectId = req.params.id;
+    let transaction;
+    try {
+      transaction = await index.sequelize.transaction();
+
+      //update todos set title = "titleA", body = "bodyA", completed = true where id = selectID;
+      const todo = await index.Todo.findById(Number(selectId), { transaction });
+      todo.update({
+        title: req.body.title,
+        body: req.body.body,
+        completed: req.body.completed
+      });
+
+      await transaction.commit();
+      res.status(200).json(todo);
+    } catch (error) {
+      await transaction.rollback();
+      res.json(error);
+    }
   },
   deleteTodos(req, res) {
     const id = req.params.id;
