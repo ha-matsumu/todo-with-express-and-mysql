68bc5b406147bc87dd5d81a5010400f02278e6e3
ha-matsumu <ha.matsumu@gmail.com>
2019-01-28 13:00:55 +0900

    modified api.router_test.js

--- test/src/resources/api.router_test.js
+++ test/src/resources/api.router_test.js
@@ -1,12 +1,6 @@
 const requestHelper = require("./requestHelper");
 
 // 正常系のテスト
-describe("PUT /api/todos/1", () => {
-  it("ステータスコード200になるはず", () => {
-    return requestHelper.requestAPI("put", "/api/todos/1", 200);
-  });
-});
-
 describe("DELETE /api/todos/1", () => {
   it("ステータスコード200になるはず", () => {
     return requestHelper.requestAPI("delete", "/api/todos/1", 200);
@@ -14,24 +8,6 @@ describe("DELETE /api/todos/1", () => {
 });
 
 // 異常系のテスト
-describe("GET /api/abc", () => {
-  it("ステータスコード404になるはず", () => {
-    return requestHelper.requestAPI("get", "/api/abc", 404);
-  });
-});
-
-describe("POST /api/abc", () => {
-  it("ステータスコード404になるはず", () => {
-    return requestHelper.requestAPI("post", "/api/abc", 404);
-  });
-});
-
-describe("PUT /api/abc", () => {
-  it("ステータスコード404になるはず", () => {
-    return requestHelper.requestAPI("put", "/api/abc", 404);
-  });
-});
-
 describe("DELETE /api/abc", () => {
   it("ステータスコード404になるはず", () => {
     return requestHelper.requestAPI("delete", "/api/abc", 404);
