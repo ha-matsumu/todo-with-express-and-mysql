2a4acddbd29cfc4c5170712397375c5a605d8ce4
ha-matsumu <ha.matsumu@gmail.com>
2019-03-07 14:47:53 +0900

    add prop-types

--- client/components/Todo/Todo.js
+++ client/components/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./Todo.css";
 
@@ -7,5 +8,12 @@ const Todo = props => (
     <h1>{props.title}</h1>
   </article>
 );
+
+Todo.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired
+};
 
 export default Todo;
