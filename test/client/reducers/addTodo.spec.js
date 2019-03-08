bd9aebb4ba60178dd8def9ecc1657499ce7279d9
ha-matsumu <ha.matsumu@gmail.com>
2019-03-08 11:06:26 +0900

    create addTodo.spec.js

---
+++ test/client/reducers/addTodo.spec.js
@@ -0,0 +1,24 @@
+import reducer from "../../../client/reducers/todos";
+import * as actionTypes from "../../../client/actions/actionTypes";
+
+describe("ADD_TODO", () => {
+  it("ADD_TODOが処理されるはず", () => {
+    expect(
+      reducer(
+        { todos: [], loading: false, error: null },
+        {
+          type: actionTypes.ADD_TODO_SUCCESS,
+          todos: { title: "demo title", body: "demo body", completed: false }
+        }
+      )
+    ).toEqual({
+      todos: {
+        title: "demo title",
+        body: "demo body",
+        completed: false
+      },
+      loading: false,
+      error: null
+    });
+  });
+});
