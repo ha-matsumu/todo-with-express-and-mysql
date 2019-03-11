ff21960557a1c5e064226a6cfb8910a06b0a4a67
ha-matsumu <ha.matsumu@gmail.com>
2019-03-11 15:24:42 +0900

    add updateTodo process

--- client/containers/TodoForm/TodoForm.js
+++ client/containers/TodoForm/TodoForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 import "./TodoForm.css";
 
@@ -7,15 +9,31 @@ class TodoForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: null,
       title: "",
       body: "",
-      completed: false
+      completed: false,
+      requestAdd: true
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange(event) {
+  componentDidUpdate() {
+    if (this.props.selectedTodoId !== this.state.id) {
+      axios.get("/api/todos/" + this.props.selectedTodoId).then(response => {
+        this.setState({
+          id: response.data.id,
+          title: response.data.title,
+          body: response.data.body,
+          completed: response.data.completed,
+          requestAdd: false
+        });
+      });
+    }
+  }
+
+  handleInputChange = event => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
@@ -23,31 +41,40 @@ class TodoForm extends Component {
     this.setState({
       [name]: value
     });
-  }
+  };
 
-  addTodoHandler = () => {
+  cancelHandler = () => {};
+
+  requestHandler = () => {
     if (!this.state.title) return;
     const todo = {
+      id: this.state.id,
       title: this.state.title,
       body: this.state.body,
       completed: this.state.completed
     };
-    this.props.addTodo(todo);
+    {
+      this.state.requestAdd
+        ? this.props.addTodo(todo)
+        : this.props.updateTodo(todo);
+    }
     this.setState({
+      id: null,
       title: "",
-      body: ""
+      body: "",
+      completed: false,
+      requestAdd: true
     });
   };
 
   render() {
     return (
       <div className="todoForm">
-        <h1>Add a Todo</h1>
+        <h1>{this.state.requestAdd ? "Add Todo" : "Update Todo"}</h1>
         <label>
           Title
           <input
             name="title"
-            type="text"
             value={this.state.title}
             onChange={this.handleInputChange}
           />
@@ -61,14 +88,31 @@ class TodoForm extends Component {
             onChange={this.handleInputChange}
           />
         </label>
-        <button onClick={this.addTodoHandler}>Add Todo</button>
+        {this.state.requestAdd ? null : (
+          <label>
+            completed
+            <select
+              name="completed"
+              value={this.state.completed}
+              onChange={this.handleInputChange}
+            >
+              <option value="false">false</option>
+              <option value="true">true</option>
+            </select>
+          </label>
+        )}
+        <button className="Cancel" onClick={this.cancelHandler}>Cancel</button>
+        <button onClick={this.requestHandler}>
+          {this.state.requestAdd ? "Add Todo" : "Update Todo"}
+        </button>
       </div>
     );
   }
 }
 
 TodoForm.propTypes = {
-  addTodo: PropTypes.func.isRequired
+  addTodo: PropTypes.func.isRequired,
+  updateTodo: PropTypes.func.isRequired
 };
 
 export default TodoForm;
