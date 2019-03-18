75365b8156aa5172750cbebd4a581609d587dd7e
ha-matsumu <ha.matsumu@gmail.com>
2019-03-18 15:49:39 +0900

    add propTypes of clickBackdrop

--- client/components/UI/Backdrop/Backdrop.js
+++ client/components/UI/Backdrop/Backdrop.js
@@ -4,10 +4,13 @@ import PropTypes from "prop-types";
 import "./Backdrop.css";
 
 const Backdrop = props =>
-  props.show ? <div className="backdrop" onClick={props.clicked} /> : null;
+  props.show ? (
+    <div className="backdrop" onClick={props.clickBackdrop} />
+  ) : null;
 
 Backdrop.propTypes = {
-  show: PropTypes.bool.isRequired
+  show: PropTypes.bool.isRequired,
+  clickBackdrop: PropTypes.func.isRequired
 };
 
 export default Backdrop;
