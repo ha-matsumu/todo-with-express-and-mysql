0f1e7a658ed57aaaa45af42c1a3a3bffa60e472a
ha-matsumu <ha.matsumu@gmail.com>
2019-03-25 13:24:18 +0900

    add order_number

--- src/models/todo.js
+++ src/models/todo.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
     title: DataTypes.STRING,
     body: DataTypes.TEXT,
-    completed: DataTypes.BOOLEAN
+    completed: DataTypes.BOOLEAN,
+    order_number: DataTypes.INTEGER
   }, {});
   Todo.associate = function(models) {
     // associations can be defined here
