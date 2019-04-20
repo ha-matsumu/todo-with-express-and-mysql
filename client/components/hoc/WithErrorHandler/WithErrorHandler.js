610397175e30375358ee1042396c41ecf4d9a71e
ha-matsumu <ha.matsumu@gmail.com>
2019-04-20 16:41:18 +0900

    create WithErrorHandler.js

---
+++ client/components/hoc/WithErrorHandler/WithErrorHandler.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import Modal from "../../UI/Modal/Modal";
+
+const WithErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        shown: false,
+        error: null
+      };
+      this.reqInterceptor = axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.resInterceptor = axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ shown: true, error: error });
+        }
+      );
+    }
+
+    // componentDidMount() {
+      
+    // }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    hideModalHandler = () => {
+      this.setState({ shown: false, error: null });
+    };
+
+    render() {
+      return (
+        <>
+          <Modal
+            shown={this.state.shown}
+            hideModalHandler={this.hideModalHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </>
+      );
+    }
+  };
+};
+
+export default WithErrorHandler;
