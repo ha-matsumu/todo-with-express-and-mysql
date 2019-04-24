8a2a7133c4a11e861d96cf8c40b149e868963da2
ha-matsumu <ha.matsumu@gmail.com>
2019-04-24 21:51:53 +0900

    move form

--- client/containers/TodoForm/AddTodoForm.js
+++ client/containers/TodoForm/AddTodoForm.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 
 import Button from "../../components/Button/Button";
 import "./TodoForm.css";
-import Spinner from "../../components/UI/Spinner/Spinner";
 
 class AddTodoForm extends Component {
   constructor(props) {
@@ -46,43 +45,37 @@ class AddTodoForm extends Component {
   };
 
   render() {
-    let form;
-
-    form = (
-      <div className="todoForm">
-        <h1>Add a Todo</h1>
-        <label>
-          Title
-          <input
-            name="title"
-            type="text"
-            value={this.state.title}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Body
-          <textarea
-            name="body"
-            rows="4"
-            value={this.state.body}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <Button btnType="cancel" clickButton={this.props.hideModalHandler}>
-          Cancel
-        </Button>
-        <Button btnType="add" clickButton={this.addTodoHandler}>
-          Add
-        </Button>
-      </div>
+    return (
+      <>
+        <div className="todoForm">
+          <h1>Add a Todo</h1>
+          <label>
+            Title
+            <input
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Body
+            <textarea
+              name="body"
+              rows="4"
+              value={this.state.body}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <Button btnType="cancel" clickButton={this.props.hideModalHandler}>
+            Cancel
+          </Button>
+          <Button btnType="add" clickButton={this.addTodoHandler}>
+            Add
+          </Button>
+        </div>
+      </>
     );
-
-    if (this.props.loading) {
-      form = <Spinner />;
-    }
-
-    return <>{form}</>;
   }
 }
 
