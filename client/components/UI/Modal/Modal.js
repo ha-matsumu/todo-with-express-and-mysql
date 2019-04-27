9d248f03e24e747813e6155e09ec43ea7931ab0b
ha-matsumu <ha.matsumu@gmail.com>
2019-04-27 14:17:11 +0900

    change class component to dumb component

--- client/components/UI/Modal/Modal.js
+++ client/components/UI/Modal/Modal.js
@@ -1,36 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <Backdrop
-          shown={this.props.shown}
-          clickBackdrop={this.props.hideModalHandler}
-        />
-        <div
-          className="modal"
-          style={{
-            transform: this.props.shown
-              ? "translateY(-100px)"
-              : "translateY(-200vh)",
-            opacity: this.props.shown ? "1" : "0"
-          }}
-        >
-          {this.props.children}
-        </div>
-      </>
-    );
-  }
-}
+const Modal = props => {
+  return (
+    <>
+      <Backdrop shown={props.shown} clickBackdrop={props.hideModalHandler} />
+      <div
+        className="modal"
+        style={{
+          transform: props.shown ? "translateY(-100px)" : "translateY(-200vh)",
+          opacity: props.shown ? "1" : "0"
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
 
 Modal.propTypes = {
   shown: PropTypes.bool.isRequired,
